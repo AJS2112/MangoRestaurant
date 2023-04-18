42833bbe89e33e6402dec6e5083f68e84c53385b
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-04-17 22:26:44 -0300

    Automapper configuration in ProductAPI

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Mango.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,10 @@ namespace Mango.Services.ProductAPI
             // Add services to the container.
 
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
