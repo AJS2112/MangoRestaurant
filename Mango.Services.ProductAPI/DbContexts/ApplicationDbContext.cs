d29173d0fd095b693cec63ddec584154dff88e84
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-04-17 22:11:14 -0300

    Create tables for Product database

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ProductAPI.DbContexts
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+        { }
 
-        }
+        public DbSet<Product> Products { get; set; }
+
     }
 }
